761ec8081f6bf2bf69899c437838e0cc91852e3b
Haroun BAKHOUCHE <haroun.bakhouche.hb@gmail.com>
2025-06-23 05:05:37 +0200

    Roboto

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const socket = io(process.env.REACT_APP_WS_URL);
 
